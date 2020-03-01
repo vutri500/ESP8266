f3c1538be5bf0d19a3df5e006069f70e17c36b39
vutri500 <61647857+vutri500@users.noreply.github.com>
2020-03-01 15:59:32 +0700

    Add files via upload

---
+++ Wifi_Station.ino
@@ -0,0 +1,24 @@
+#include<ESP8266WiFi.h>
+
+const char* ssid = "TranYen";
+const char* password = "46512345";
+
+void setup(void){
+  delay(1000);
+  Serial.begin(115200);
+  Serial.printf("\nConnecting to %s \n", ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    }
+
+  Serial.println();
+  Serial.printf("Connected, IP address:");
+  Serial.println(WiFi.localIP());
+  }
+
+void loop(){
+  }
